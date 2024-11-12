69f1c63075d03069ac7878fc4100a96e22b6add2
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-12 11:51:31 +0100

    added design sketch for page header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isSideMenuOpen = false;
+  isDark: boolean = this.getThemeFromLocalStorage();
+
+  constructor() {}
+
+  /**
+   * Retrieves the theme preference from local storage or the user's system settings.
+   *
+   * @returns {boolean} - `true` if the theme is dark, otherwise `false`.
+   */
+  getThemeFromLocalStorage(): boolean {
+    if (localStorage.getItem('isDark') != null) {
+      return localStorage.getItem('isDark') === true.toString();
+    }
+
+    // check user's system theme
+    return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  }
+
+  /**
+   * Toggles the state of the side menu.
+   * If the side menu is currently open, it will be closed.
+   * If the side menu is currently closed, it will be opened.
+   */
+  toggleSideMenu(): void {
+    this.isSideMenuOpen = !this.isSideMenuOpen;
+  }
+
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    localStorage.setItem('isDark', this.isDark.toString());
+  }
 
 }
