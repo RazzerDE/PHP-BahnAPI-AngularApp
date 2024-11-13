192abc007c36f848e120b78a5ee85adc803233c7
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-13 11:45:30 +0100

    added content to start page (alert & info-box)

--- src/app/pages/start-main/start-main.component.ts
+++ src/app/pages/start-main/start-main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-start-main',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './start-main.component.html',
   styleUrl: './start-main.component.css'
 })
