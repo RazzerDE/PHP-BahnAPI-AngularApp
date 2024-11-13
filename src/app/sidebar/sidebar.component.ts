dc2e08b544e8e59fa64d013ebd7b5af69b765b56
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-13 10:48:35 +0100

    added sidebar animation for active page & fixed font color not changing for active page

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {NgClass, NgOptimizedImage} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-  protected readonly document = document;
+export class SidebarComponent implements OnInit{
+  isLoaded: boolean = false;
+
+  constructor(protected router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoaded = true; // show animation for sidebar active page
+    }, 50); // Small delay to ensure DOM is ready and animation triggers
+  }
 }
